524b6d5e2058271ef82919902860a9c13ef38b85
TJ2Robotics <navtges@gmail.com>
2016-01-30 09:45:16 -0800

    cleanup

--- src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -11,18 +11,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  *
  */
 public class Drive extends Subsystem {
-	private final CANTalon lTalonMaster, lTalonSlave1, lTalonSlave2, rTalonMaster, rTalonSlave1, rTalonSlave2 ;
+	private final CANTalon lTalonMaster, lTalonSlave1, rTalonMaster, rTalonSlave1;
 
 	public Drive() {
+		// set up drive masters
 		lTalonMaster = new CANTalon(RobotMap.leftMotorController1);
 		lTalonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
 
-	//	lTalonMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
-
 		rTalonMaster = new CANTalon(RobotMap.rightMotorController1);
 		rTalonMaster.setFeedbackDevice(CANTalon.FeedbackDevice.QuadEncoder);
-
-	//	rTalonMaster.changeControlMode(CANTalon.TalonControlMode.PercentVbus);
 
 		// set up drive slaves
 		lTalonSlave1 = new CANTalon(RobotMap.leftMotorController2);
@@ -32,17 +29,6 @@ public class Drive extends Subsystem {
 		rTalonSlave1 = new CANTalon(RobotMap.rightMotorController2);
 		rTalonSlave1.changeControlMode(CANTalon.TalonControlMode.Follower);
 		rTalonSlave1.set(rTalonMaster.getDeviceID());
-
-		
-		lTalonSlave2 = new CANTalon(RobotMap.leftMotorController3);
-		lTalonSlave2.changeControlMode(CANTalon.TalonControlMode.Follower);
-		lTalonSlave2.set(lTalonMaster.getDeviceID());
-
-		rTalonSlave2 = new CANTalon(RobotMap.rightMotorController3);
-		rTalonSlave2.changeControlMode(CANTalon.TalonControlMode.Follower);
-		rTalonSlave2.set(rTalonMaster.getDeviceID());
-		
-
 	}
 	
 	public void move (double left, double right){
@@ -57,24 +43,16 @@ public class Drive extends Subsystem {
 	
 	private void updateSmartDashboard() {
 		SmartDashboard.putNumber("Left Encoder: ", lTalonMaster.getEncPosition());
-		SmartDashboard.putNumber("Left Encoder Quad A: ", lTalonMaster.getPinStateQuadA());
-		SmartDashboard.putNumber("Left Encoder Quad B: ", lTalonMaster.getPinStateQuadB());
-		SmartDashboard.putNumber("Left Encoder Quad Idx: ", lTalonMaster.getPinStateQuadIdx());
-		
 		SmartDashboard.putNumber("Left Master Voltage: ", lTalonMaster.getOutputVoltage());
 		SmartDashboard.putNumber("Left Master Current: ", lTalonMaster.getOutputCurrent());
 		SmartDashboard.putNumber("Left Slave 1 Voltage: ", lTalonSlave1.getOutputVoltage());
 		SmartDashboard.putNumber("Left Slave 1 Current: ", lTalonSlave1.getOutputCurrent());
-		SmartDashboard.putNumber("Left Slave 2 Voltage: ", lTalonSlave2.getOutputVoltage());
-		SmartDashboard.putNumber("Left Slave 2 Current: ", lTalonSlave2.getOutputCurrent());
 
 		SmartDashboard.putNumber("Right Encoder: ", rTalonMaster.getEncPosition());
 		SmartDashboard.putNumber("Right Master Voltage: ", rTalonMaster.getOutputVoltage());
 		SmartDashboard.putNumber("Right Master Current: ", rTalonMaster.getOutputCurrent());
 		SmartDashboard.putNumber("Right Slave 1 Voltage: ", rTalonSlave1.getOutputVoltage());
 		SmartDashboard.putNumber("Right Slave 1 Current: ", rTalonSlave1.getOutputCurrent());
-		SmartDashboard.putNumber("Right Slave 2 Voltage: ", rTalonSlave2.getOutputVoltage());
-		SmartDashboard.putNumber("Right Slave 2 Current: ", rTalonSlave2.getOutputCurrent());
 	}
 	
 	public void initDefaultCommand() {
